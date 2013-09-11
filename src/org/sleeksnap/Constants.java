1662dfdbd13bf1ea60c387b69583bcb60efdcc1d
Nikki <nikki@nikkii.us>
2013-09-11 11:52:39 -0400

    Increase version for auto update

--- src/org/sleeksnap/Constants.java
+++ src/org/sleeksnap/Constants.java
@@ -42,7 +42,7 @@ public class Constants {
 	public static class Version {
 		public static final int MAJOR = 1;
 		public static final int MINOR = 3;
-		public static final int PATCH = 3;
+		public static final int PATCH = 4;
 		
 		private static String versionString = null;
 		
