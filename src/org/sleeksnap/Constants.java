ba35df253eb7c8685b1ea17e17f9a7a8a02ca9f1
nikkiii <nikki@nikkii.us>
2013-10-17 21:28:08 -0400

    Raise the version number

--- src/org/sleeksnap/Constants.java
+++ src/org/sleeksnap/Constants.java
@@ -50,7 +50,7 @@ public class Constants {
 	public static class Version {
 		public static final int MAJOR = 1;
 		public static final int MINOR = 3;
-		public static final int PATCH = 7;
+		public static final int PATCH = 8;
 		
 		private static String versionString = null;
 		
