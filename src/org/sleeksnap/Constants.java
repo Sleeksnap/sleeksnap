656c0b49ba24d04e76064da5edf5eafcc88efd21
nikkiii <nikki@nikkii.us>
2013-11-22 16:17:20 -0500

    Increase version

--- src/org/sleeksnap/Constants.java
+++ src/org/sleeksnap/Constants.java
@@ -56,7 +56,7 @@ public class Constants {
 	public static class Version {
 		public static final int MAJOR = 1;
 		public static final int MINOR = 3;
-		public static final int PATCH = 15;
+		public static final int PATCH = 16;
 		
 		private static String versionString = null;
 		
